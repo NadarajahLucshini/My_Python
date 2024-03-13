5d5295c8650b5e4d9b4938f858d22d7834fcc0c7
nadarajah Lucshin <nadarajahlucshini@gmail.com>
2024-03-13 14:07:36 +0530

    first commit

---
+++ loop.py
@@ -0,0 +1,24 @@
+i =0
+while i<10:
+    print(i)
+    if (i==5):
+        break
+    i = i+1
+
+print ()
+print()
+for x in range(2, 6):
+  print(x)
+
+print ()
+print()
+
+for x in "university":
+    print (x)
+
+
+print ()
+print()
+for x in range(1,10,1):
+  print(x)
+    
